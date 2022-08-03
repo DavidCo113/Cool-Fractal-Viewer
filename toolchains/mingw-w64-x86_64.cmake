b9815e7d6e9796c94fe43d61f436bb1dde3cfc3d
David Cole <davidco7777@protonmail.com>
2022-08-02 21:27:48 -0600

    hopefully this fixes windows builds

--- toolchains/mingw-w64-x86_64.cmake
+++ toolchains/mingw-w64-x86_64.cmake
@@ -7,14 +7,21 @@
 
 set(CMAKE_SYSTEM_NAME Windows)
 set(CMAKE_SYSTEM_PROCESSOR AMD64)
-#set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -lmingw32 -lSDL2main -lSDL2 -mwindows -lm -ldinput8 -ldxguid -ldxerr8 -luser32 -lgdi32 -lwinmm -limm32 -lole32 -loleaut32 -lshell32 -lversion -luuid -static-libgcc -static-libstdc++")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -static-libgcc -static-libstdc++")
 set(TOOLCHAIN_PREFIX x86_64-w64-mingw32)
 
 # cross compilers to use for C, C++ and Fortran
-set(CMAKE_C_COMPILER ${TOOLCHAIN_PREFIX}-gcc)
-set(CMAKE_CXX_COMPILER ${TOOLCHAIN_PREFIX}-g++)
-set(CMAKE_Fortran_COMPILER ${TOOLCHAIN_PREFIX}-gfortran)
+if (EXISTS ${TOOLCHAIN_PREFIX}-gcc-posix)
+    set(CMAKE_CXX_COMPILER ${TOOLCHAIN_PREFIX}-gcc-posix)
+else()
+    set(CMAKE_CXX_COMPILER ${TOOLCHAIN_PREFIX}-gcc)
+endif()
+
+if (EXISTS ${TOOLCHAIN_PREFIX}-g++-posix)
+    set(CMAKE_CXX_COMPILER ${TOOLCHAIN_PREFIX}-g++-posix)
+else()
+    set(CMAKE_CXX_COMPILER ${TOOLCHAIN_PREFIX}-g++)
+endif()
 set(CMAKE_RC_COMPILER ${TOOLCHAIN_PREFIX}-windres)
 
 # target environment on the build host system
