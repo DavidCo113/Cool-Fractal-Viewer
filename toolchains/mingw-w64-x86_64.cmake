d9ff19fde4279c9990f92cdd62bc5a17a2900754
DavidCo113 <davidco7777@protonmail.com>
2022-08-03 03:48:42 +0000

    Update mingw-w64-x86_64.cmake

--- toolchains/mingw-w64-x86_64.cmake
+++ toolchains/mingw-w64-x86_64.cmake
@@ -9,7 +9,6 @@ set(CMAKE_SYSTEM_NAME Windows)
 set(CMAKE_SYSTEM_PROCESSOR AMD64)
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -static-libgcc -static-libstdc++")
 set(TOOLCHAIN_PREFIX x86_64-w64-mingw32)
-set(CMAKE_GNULD_IMAGE_VERSION "-Wl")
 
 # cross compilers to use for C, C++ and Fortran
 if (EXISTS ${TOOLCHAIN_PREFIX}-gcc-posix)
